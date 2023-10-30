c4d00ae143ebdd25ce9423f0a05fbcc9e003011d
minjyun3011 <grand3canyon10@gmail.com>
2023-10-30 11:17:23 +0900

    データの保存まで完了！

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.show', ['task' => $task]);
+    }
+    public function create()
+    {
+        return view('tasks.index');
+    }
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->title = $request->title;
+        $task->body = $request->body;
+        $task->save();
+        return redirect(route("tasks.index"));
+    }
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>localhost/tasks</title>
+</head>
+
+<body>
+    <h1>タスク一覧</h1>
+    @foreach ($tasks as $task)
+        <a href="{{ route('tasks.show', $task) }}">{{ $task->title }}</a>
+    @endforeach
+    <hr>
+    <h1>新規論文投稿</h1>
+    <form action="{{ route('tasks.store') }}" method="post">
+        @csrf
+        <p>
+            <label for="title">タイトル</label><br>
+            <input type="text" name="title" id="title">
+        </p>
+        <p>
+            <label for="body">内容</label><br>
+            <textarea name="body" class="body" id="body"></textarea>
+        </p>
+
+        <input type="submit" value="Create Task">
+    </form>
+</body>
+
+</html>
